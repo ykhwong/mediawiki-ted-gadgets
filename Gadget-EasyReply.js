3066d2347e21fec34224ffa58eb4106f7ec3fa36
ted <ykhwong@daum.net>
2020-01-26 01:05:21 +0900

    [ADD] Threaded discussion

--- Gadget-EasyReply.js
+++ Gadget-EasyReply.js
@@ -2,6 +2,7 @@
 * EasyReply
 * @author ykhwong
 * Please note that this is a beta version of the Easy-Reply.
+* TO-DO: Needs to clean up
 */
 
 const affectedPages = [
@@ -15,31 +16,134 @@ const affectedPages = [
 
 $(window).on('pageshow',function(){
 	function addMarginIcon() {
-		$(".mw-parser-output dd").each(function() {
-			var txt = $(this).html().replace(/<(dl|dd)>.*/ig, "");
-			if (/\S/.test(txt)) {
-				$(this).prepend('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/OOjs_UI_indicator_arrow-ltr-progressive.svg/20px-OOjs_UI_indicator_arrow-ltr-progressive.svg.png">');
+		var skip = false;
+
+		$(".mw-parser-output h2, .mw-parser-output dd").each(function() {
+			if ($(this).is("h2")) {
+				skip = true;
+				return true;
 			}
+			if (!skip) {
+				return true;
+			}
+			if ($(this).is("dd")) {
+				var txt = $(this).html().replace(/<(dl|dd)>.*/ig, "");
+				if (/\S/.test(txt)) {
+					if (/<(dl|dd)>/.test($(this).html())) {
+						$(this).html(
+							'<div class="css1">&nbsp;' +
+							$(this).html().replace(/<(dl|dd)>/, '</div><$1>')
+						);
+					}
+				}
+			}
+		});
+
+		skip = false;
+		$(".mw-parser-output h2, .mw-parser-output dd, .mw-parser-output dt, .mw-parser-output ol li, .mw-parser-output ul li, .mw-parser-output p")
+		.not("li li").not("li p").not("table li").not("#toc ul li").not(".hlist ul li").each(function() {
+			if ($(this).is("h2")) {
+				skip = true;
+				return true;
+			}
+			if (!skip) {
+				return true;
+			}
+			if ($(this).is("dd")) {
+				var txt = $(this).html().replace(/<(dl|dd)>.*/ig, "");
+				if (/\S/.test(txt)) {
+					if (/<(dl|dd)>/.test($(this).html())) {
+						//$(this).html(
+						//	'<div class="css1">&nbsp;' +
+						//	$(this).html().replace(/<(dl|dd)>/, '</div><$1>')
+						//);
+					} else {
+						$(this).html(
+							$(this).html().replace(/(.*)/, '<div class="css1">$1<div>')
+						);
+					}
+				}
+			} else if ($(this).is("dt")) {
+				if (/\S/.test($(this).text())) {
+					$(this).addClass("css1");
+					if ($(this).prev().is("dt") || $(this).next().is("dt")) {
+						$(this).addClass("cssp");
+					}
+				}
+			} else if ($(this).is("li")) {
+				if (/\S/.test($(this).text())) {
+					$(this).addClass("css1");
+					$(this).addClass("css2");
+					if ($(this).prev().is("li") || $(this).next().is("li")) {
+						$(this).addClass("cssp");
+						if ($(this).is("ol li")) {
+							$(this).css("margin-left", "-45px");
+						}
+					}
+				}
+			} else if ($(this).is("p")) {
+				if (/\S/.test($(this).text())) {
+					$(this).addClass("css1");
+					if ($(this).prev().is("p") || $(this).next().is("p")) {
+						if (!/(\(KST\)|서명을 남기지 않아 다른 사용자가 추가하였습니다\.)\s*$/.test($(this).text())) {
+							$(this).addClass("cssp");
+						} else {
+							$(this).css("margin-top", "-1.5px");
+						}
+					}
+				}
+			}
+		});
+
+		$(".css1").css({
+			"height": "100%",
+			"border-style": "solid",
+			"border-width": "0px 1px 0px 8px",
+			"padding": "12px",
+			"word-break": "break-all",
+			"border-color": "DodgerBlue",
+			"background-color":"rgba(228, 240, 255, 1)"
+		});
+		$(".css2").css({
+			"list-style-position": "inside",
+			"padding-top": "5px",
+			"padding-bottom": "5px"
+		});
+		$(".cssp").css({
+			"margin-top": "-1.5px",
+			"margin-bottom": "-1.5px"
 		});
 	}
 
 	function addReplyIcon() {
 		var editLink = "";
 
-		$(".mw-editsection a, .mw-parser-output p, .mw-parser-output dd, .mw-parser-output li").each(function() {
+		$(".mw-editsection a, .mw-parser-output p, .mw-parser-output dd, .mw-parser-output li")
+		.each(function() {
 			if ($(this).attr("href")) {
 				editLink = $(this).attr("href");
 			}
 
+			if ($(this).find("blockquote").length > 0) {
+				return true;
+			}
+
 			if ($(this).find("dl").length > 0) {
-				//console.log("★" + $(this).text());
 			} else {
 				if (/(\(KST\)|서명을 남기지 않아 다른 사용자가 추가하였습니다\.)\s*$/.test($(this).text())) {
 					// TO-DO: Needs to be replaced with addReplayBox()
-					$(this).append("&nbsp;<a href='#'" +
+					var tmpHtml = "&nbsp;<a href='#'" +
 					'onClick="var replyPopup=window.open(' + "'" + 
 					"https://ko.m.wikipedia.org" +
-					editLink + "','replyPopup','width=600,height=500'); var win_timer = setInterval(function() { if(replyPopup.closed) { window.location.reload(); clearInterval(win_timer); } }, 100);  return false;" + '"' +"><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Ic_reply_48px.svg/20px-Ic_reply_48px.svg.png'></a>");
+					editLink + "','replyPopup','width=600,height=500'); var win_timer = setInterval(function() { if(replyPopup.closed) { window.location.reload(); clearInterval(win_timer); } }, 100);  return false;" + '"' +"><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Ic_reply_48px.svg/20px-Ic_reply_48px.svg.png'></a>";
+
+					if ($(this).is("dd")) {
+						$(this).html(
+							$(this).html().replace(/<div>/, tmpHtml + "<div>")
+						);
+					} else {
+						$(this).append(tmpHtml);
+					}
 				}
 			}
 		});
@@ -111,7 +215,7 @@ $(window).on('pageshow',function(){
 	function main() {
 		var currentPageName = mw.config.get( 'wgPageName' );
 		var currentNamespace = mw.config.get( 'wgNamespaceNumber' );
-		
+
 		if (/^(1|3|5|7|9|11|13|15|101|103|119|829|2301|2303)$/.test(currentNamespace)) {
 			run();
 			return;
