a2092d1d9c1c7ad1c5cce31ae407690cf569adf7
ted <ykhwong@daum.net>
2020-08-05 14:36:41 +0900

    [ADD] Gadget-CategoryTop

---
+++ Gadget-CategoryTop.js
@@ -0,0 +1,9 @@
+/*
+* CategoryTop
+* @author ykhwong
+*/
+$(function () {
+	$(".mw-parser-output").prepend($("#catlinks"));
+	$("#catlinks").css("margin-bottom", "15px");
+	$("#catlinks ul").css("margin-left", "0px");
+}());
