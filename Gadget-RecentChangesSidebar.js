43f3e07294e57aac3c231ed76d66c74fbd5b5329
ted <ykhwong@daum.net>
2020-08-09 20:14:28 +0900

    [ADD] Add repos()

--- Gadget-RecentChangesSidebar.js
+++ Gadget-RecentChangesSidebar.js
@@ -6,13 +6,12 @@ $(function () {
 	const sidebarWidth = 200;
 	var rcText = "";
 	var rcSidebarStyle = {
-		"position": "fixed",
-		"float": "right",
+		"position": "absolute",
 		"backgroundColor": "#b5e1ff",
 		"color": "black",
 		"width": sidebarWidth + "px",
-		"top": "200px",
-		"right": "20px",
+		"top": "0px",
+		"right": "0px",
 		"padding": "3px",
 		"border": "solid 2px #7ec9fc"
 	};
@@ -22,20 +21,35 @@ $(function () {
 		"padding": "5px",
 		"margin": "-5px -5px 3px -5px"
 	};
-	var rcSidebarCollapsed = false;
-	if (mw.config.get("wgVectorDisableSidebarPersistence") === null) {
-		$("#mw-content-text").css("margin-right", (sidebarWidth + 20) + "px");
-	}
-	$("body").append('<div id="rcSidebar"></div>');
+	$("#mw-content-text").append('<div id="rcSidebar"></div>');
 	$("#rcSidebar").css(rcSidebarStyle);
+	if (mw.config.get("wgVectorDisableSidebarPersistence") === null || mw.config.get("wgNamespaceNumber") === -1) {
+		$("#mw-content-text").css("margin-right", (sidebarWidth + 30) + "px");
+	} else {
+		repos();
+	}
 	if (localStorage['mw-recentchanges-sidebar']  !== undefined) {
 		if (localStorage['mw-recentchanges-sidebar-tab1'] !== undefined) {
 			rcText = localStorage['mw-recentchanges-sidebar-tab1'];
-			$("#rcSidebar").html('<div class="rcSidebarTab"><span style="font-weight: bold;">' + rcText + '</span></div>');
+			addRcText();
 			$("#rcSidebar").append(localStorage['mw-recentchanges-sidebar']);
 		}
 	}
 	$(".rcSidebarTab").css(rcSidebarTabStyle);
+
+	function addRcText() {
+		$("#rcSidebar").html('<div class="rcSidebarTab" style="font-weight: bold;"><a href="/wiki/Special:RecentChanges">' + rcText + '</a></div>');
+	}
+
+	function repos() {
+		if ($(".mw-workspace-container").outerWidth() < 1440) {
+			$("#rcSidebar").css("margin-right", "0px");
+			$("#mw-content-text").css("margin-right", (sidebarWidth + 30) + "px");
+		} else {
+			$("#rcSidebar").css("margin-right", -1 * (sidebarWidth + 90) + "px");
+			$("#mw-content-text").css("margin-right", "0px");
+		}
+	}
 
 	function refresh() {
 		if (document.hidden || document.msHidden || document.webkitHidden || document.mozHidden) {
@@ -48,7 +62,7 @@ $(function () {
 			var special = $(data).find(".special");
 			rcText = $(data).find("#firstHeading").text();
 			localStorage['mw-recentchanges-sidebar-tab1'] = rcText;
-			$("#rcSidebar").html('<div class="rcSidebarTab"><span style="font-weight: bold;">' + rcText + '</span></div>');
+			addRcText();
 			$(".rcSidebarTab").css(rcSidebarTabStyle);
 			localStorage['mw-recentchanges-sidebar'] = "";
 			special.children().each(function() {
@@ -56,10 +70,11 @@ $(function () {
 				var targetPage = elem.find(".mw-changeslist-line-inner").data("target-page");
 				var changedDate = elem.find(".mw-changeslist-date").text();
 				var info = 
-					'<div title="' + targetPage + '" style="display:inline-block; width: ' + (sidebarWidth - 50) + 'px; white-space: nowrap; overflow: hidden; vertical-align: text-top;"><a href="/wiki/' + targetPage + '">' +
-					targetPage + '</a></div>' +
-					'&nbsp;<div style="display:inline-block; white-space: nowrap; padding-left: 5px; color:green; font-size:smaller; vertical-align: text-top;">' + changedDate + "</div>" +
-					'<br />';
+					'<div title="' + targetPage + '" style="display:inline-block; width: ' +
+					(sidebarWidth - 40) + 'px; white-space: nowrap; overflow: hidden; vertical-align: text-top;">' +
+					'<a href="/wiki/' + targetPage + '">' + targetPage + '</a></div>' +
+					'<div style="display:inline-block; white-space: nowrap; padding-left: 5px; color:green; font-size:smaller; vertical-align: text-top;">' +
+					changedDate + "</div>" + '<br />';
 				localStorage['mw-recentchanges-sidebar'] += info;
 				$("#rcSidebar").append(info);
 			});
@@ -69,33 +84,9 @@ $(function () {
 		});
 	}
 	refresh();
-	if (mw.config.get("wgVectorDisableSidebarPersistence") !== null) {
+	if (mw.config.get("wgVectorDisableSidebarPersistence") !== null && mw.config.get("wgNamespaceNumber") !== -1) {
 		$(window).resize(function() {
-			if (mw.config.get("wgNamespaceNumber") === -1) {
-				if ($("body").outerWidth() > 1885) {
-					if (rcSidebarCollapsed) {
-						$("#rcSidebar").show();
-					}
-					rcSidebarCollapsed = false;
-				} else {
-					if (!rcSidebarCollapsed) {
-						$("#rcSidebar").hide();
-					}
-					rcSidebarCollapsed = true;
-				}
-				return;
-			}
-			if ($(".mw-workspace-container").outerWidth() > 1358) {
-				if (rcSidebarCollapsed) {
-					$("#rcSidebar").show();
-				}
-				rcSidebarCollapsed = false;
-			} else {
-				if (!rcSidebarCollapsed) {
-					$("#rcSidebar").hide();
-				}
-				rcSidebarCollapsed = true;
-			}
+			repos();
 		});
 	}
 }());
