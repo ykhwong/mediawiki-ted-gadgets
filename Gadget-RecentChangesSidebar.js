1297a0ea5d302316c87e07c1e253e06dd81430c4
ted <ykhwong@daum.net>
2020-08-09 21:58:09 +0900

    [FIX] Check .iw-resultset

--- Gadget-RecentChangesSidebar.js
+++ Gadget-RecentChangesSidebar.js
@@ -27,6 +27,9 @@ $(function () {
 	if (mw.config.get("wgVectorDisableSidebarPersistence") === null || mw.config.get("wgNamespaceNumber") === -1
 	|| mw.config.get("wgAction") === "history") {
 		$("#mw-content-text").css("margin-right", (sidebarWidth + 30) + "px");
+		if ($(".iw-resultset").length > 0) {
+			$("#rcSidebar").css("margin-right", (-1 * sidebarWidth - 30) + "px");
+		}
 	} else {
 		repos();
 	}
