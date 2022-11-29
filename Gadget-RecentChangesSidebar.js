07ac877a8f21f3d2e4716667f34beb261db34238
ted <ykhwong@daum.net>
2022-11-29 13:00:24 +0900

    [ADD] Add diffLink

--- Gadget-RecentChangesSidebar.js
+++ Gadget-RecentChangesSidebar.js
@@ -276,12 +276,13 @@ $(function () {
 				var elem = $(this);
 				var targetPage = elem.find(".mw-changeslist-line-inner").data("target-page");
 				var changedDate = elem.find(".mw-changeslist-date").text();
+				var diffLink = $(elem.find(".mw-changeslist-links > span > a:first")[0]).attr('href');
 				var info = 
 					'<div title="' + targetPage + '" style="display: inline-block; width: ' +
 					(sidebarWidth - 40) + 'px; white-space: nowrap; overflow: hidden; margin-left: 2px; vertical-align: text-top;">' +
 					'<a href="/wiki/' + encodeURIComponent(targetPage) + '">' + targetPage + '</a></div>' +
 					'<div style="display:inline-block; white-space: nowrap; padding-left: 5px; color:black; font-size:smaller; vertical-align: text-top;">' +
-					changedDate + "</div>" + '<br />';
+					'<a href=' + diffLink + '</a>' + changedDate + "</a></div>" + '<br />';
 				localStorage['mw-recentchanges-sidebar'] += info;
 				$("#rcSidebar").append(info);
 			});
